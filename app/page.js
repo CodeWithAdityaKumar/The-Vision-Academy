c9f66b301e3e6ab71841cb8bb485df782c05b8bb
CodeWithAdityaKumar <adityakumar7079629108@gmail.com>
2025-01-09 17:08:34 +0530

    Books and Notes

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@ import AboutSection from '../components/AboutSection';
 import CoursesSection from '../components/CoursesSection';
 import TeachersSection from '../components/TeachersSection';
 import ContactSection from '../components/ContactSection';
-import Footer from '../components/Footer';
-
 export default function Home() {
   return (
     <div>
